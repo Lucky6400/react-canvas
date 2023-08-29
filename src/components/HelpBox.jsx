3a982962f551c2099df9e22cc5d58154c73814ca
Lucky Jain <luckyjain78281@gmail.com>
2023-08-30 01:28:47 +0530

    some changes

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react"
 
 const HelpBox = () => {
+  const [display, setDisplay] = useState(false);
+
   return (
-    <div className="helpBox">
-        HOW TO USE: <br />
-        1) Triple Click on image and drag to create a bounding box. <br />
-        2) Click on bounding box to make changes. <br />
-        3) Save or undo changes as and when required on clicking the buttons beside the delete button. <br />
-        4) Delete any box by clicking on it and then clicking delete button. <br />
-        5) Download annotations by clicking download button, this will download in the form of JSON file. <br />
-        6) Click anywhere on image to unselect a bounding box.
-      </div>
+    <>
+      {display ?
+        <div className="helpBox">
+          HOW TO USE: <br />
+          1) Triple Click on image and drag to create a bounding box. <br />
+          2) Click on bounding box to make changes. <br />
+          3) Save or undo changes as and when required on clicking the buttons beside the delete button. <br />
+          4) Delete any box by clicking on it and then clicking delete button. <br />
+          5) Download annotations by clicking download button, this will download in the form of JSON file. <br />
+          6) Click anywhere on image to unselect a bounding box.
+        </div>
+        : <></>
+
+      }
+      <button
+        onClick={() => setDisplay(p => !p)}
+        className="helpBtn">Help</button>
+    </>
   )
 }
 
